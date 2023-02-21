c7f41b82705bf6a82863b395ddd517870096903c
kimballer7 <kimballowen.is@gmail.com>
2023-02-21 14:22:51 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mission08_3_12.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
